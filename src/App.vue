dd5fb5da27a03552817bee92673fa0d2ffca59cf
maruihua <amaruihua_v@didiglobal.com>
2022-03-30 11:04:35 +0800

    使用 ref computed, ts中要使用vue2的API，需要用defineComponent包裹

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <button @click="increase">👍 + 1</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import {defineComponent, ref, computed} from 'vue';
+// Vue2写法
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    // 无法访问THIS,
+    const count = ref(0); // 响应式对象： 监测到改变后做出响应
+    const increase = () => {
+      count.value ++
+    }
+    const double = computed(()=>{return count.value * 2})
+    return {
+      count,
+      increase,
+      double
+    }
+  },  
+  data(){
+    // return {
+    //   count: 0
+    // }
+  },
+  methods:{
+    // increase(){
+    //   this.count ++
+    // }
   }
-});
+})
 </script>
 
 <style>
