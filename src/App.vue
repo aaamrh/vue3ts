75490975f0ca3e6c4877e84a954b1dc99913ac8a
maruihua <amaruihua_v@didiglobal.com>
2022-03-30 11:18:56 +0800

    reactive toRefs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <!-- <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1> -->
   <h1>{{ count }}</h1>
   <h1>{{ double }}</h1>
   <button @click="increase">👍 + 1</button>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, computed} from 'vue';
+import {defineComponent, ref, computed, reactive, toRefs} from 'vue';
+interface IDataProps {
+  count: number,
+  double: number,
+  increase: ()=>void
+}
 // Vue2写法
 export default defineComponent({
   name: 'App',
   setup(){
     // 无法访问THIS,
-    const count = ref(0); // 响应式对象： 监测到改变后做出响应
-    const increase = () => {
-      count.value ++
-    }
-    const double = computed(()=>{return count.value * 2})
+    // const count = ref(0); // 响应式对象： 监测到改变后做出响应
+    // const increase = () => {
+    //   count.value ++
+    // }
+    // const double = computed(()=>{return count.value * 2})
+    const data:IDataProps = reactive({
+      count:0, 
+      increase: ()=>{ data.count++ },
+      double: computed(()=>data.count * 2)
+    })
+    const refData = toRefs(data) // 使用toRefs解决丧失响应性的问题
     return {
-      count,
-      increase,
-      double
+      // count,
+      // increase,
+      // double
+      ...refData
     }
   },  
   data(){
