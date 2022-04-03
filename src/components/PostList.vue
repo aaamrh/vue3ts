b300fd0ad535bcdda529c1a4f31717b7e930f63a
maruihua <marh@qingtong123.com>
2022-04-03 20:14:37 +0800

    feat: PostList

---
+++ src/components/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="post-list">
+    <article v-for="post in list" :key="post._id" class="card mb-3 shadow-sm">
+      <div class="card-body">
+        <h4>{{post.title}}</h4>
+        <div class="row my-3 align-items-center">
+          <div v-if="post.image" class="col-3">
+            <img :src="post.image" alt="" class="rounded-lg">
+          </div>
+          <p :class="{'col-9': post.image}">{{post.content}}</p>
+        </div>
+        <span class="test-muted">{{post.createdAt}}</span>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue';
+import { PostProps } from '../testData';
+
+export default defineComponent({
+  props: {
+    list: {
+      required: true,
+      type: Array as PropType<PostProps[]>
+    }
+  },
+  setup(){
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+.post-list h4 a {
+  text-decoration: none;
+  color:#1a1a1a;
+}
+.post-list h4 a:hover {
+  color:#0d6efd;
+}
+</style>
